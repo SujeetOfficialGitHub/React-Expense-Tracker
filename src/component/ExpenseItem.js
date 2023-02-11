f06ef06298328f04f9eefa0c686113d6a4f89502
Sujeet Kushwaha <sujeetgroups@gmail.com>
2023-02-11 09:30:13 +0530

    initial

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <>
+        <div>
+            <h3>Expense Items</h3>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </div>
+    </>
+  )
+}
+
+export default ExpenseItem
