639ca5695c9cfe10bf237c001470094c3a0f788b
Sujeet Kushwaha <sujeetgroups@gmail.com>
2023-02-13 07:17:16 +0530

    Adiing a filter option by year

--- src/component/expenses/Expenses.js
+++ src/component/expenses/Expenses.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
-
+import ExpenseFilter from './ExpenseFilter';
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
   return (
         <div className="expenses">
+          <ExpenseFilter selected={filteredYear} onchange={filterChangeHandler} />
         {props.expensesList.map((expenseItem,i) => (
             <ExpenseItem key={expenseItem.id} expenseItem={expenseItem}/>
         ))}
