9ed5782437a15993f82922aa7502af562893315e
Sujeet Kushwaha <sujeetgroups@gmail.com>
2023-02-13 19:16:33 +0530

    Read form data and print in console using State

--- src/component/newExpenses/ExpenseForm.js
+++ src/component/newExpenses/ExpenseForm.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpensesForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredamount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleHandler = (e) => {
+        setEnteredTitle(e.target.value)
+    }
+    const amountHandler = (e) => {
+        setEnteredAmount(e.target.value)
+        
+    }
+    const dateHandler = (e) => {
+        setEnteredDate(e.target.value)
+
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const enteredData = {
+            title : enteredTitle,
+            amount : enteredamount,
+            date : enteredDate
+        }
+        console.log(enteredData)
+        
+    }
   return (
     <>
-        <form id='input-form' action="">
+        <form onSubmit={submitHandler} action="">
             <div className="input-box">
                 <label htmlFor="title">Title</label><br />
-                <input type="text"  />
+                <input type="text" value={enteredTitle} onChange={titleHandler} />
             </div>
             <div className="input-box">
                 <label htmlFor="amount">Amount</label><br />
-                <input type="number"  />
+                <input type="number" value={enteredamount} onChange={amountHandler} />
             </div>
             <div className="input-box">
                 <label htmlFor="date">Date</label><br />
-                <input type="date"  />
+                <input type="date" value={enteredDate} onChange={dateHandler} />
             </div>
             <div className="btn">
                 <input className='submit-btn' type="submit" value="Submit" />
